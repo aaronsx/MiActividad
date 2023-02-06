73be332c59d1bc9611d481cd0babb9658059a228
aaronsx <senen.00@gmail.com>
2023-02-06 13:09:13 +0100

    Poniendo valores

--- Actividad/src/main/webapp/script/actividad.js
+++ Actividad/src/main/webapp/script/actividad.js
@@ -1,12 +1,13 @@
 alert(document.lastModified);
 function ejemplo1()
 {
-	var nombre = "Jose";
-	var apellido ="Garcia";
-	let media=(4.5+2.1);
-	document.cookie = "nombre=Jose";
-	document.cookie = "apellido=Garcia";
 	
+	var apellido =document.getElementById("apellit").value;
+	 var nombre = document.getElementById("nombret").value;
+
+	let media=(4.5+2.1);
+	document.cookie = "nombre="+nombre;
+	document.cookie = "apellido="+apellido;
 	
 	document.write("Alumno: "+nombre.charAt(0)+"."+apellido.charAt(0)+". Con media: "+media/2+" Es de tipo: "+typeof(media)+" ");
 	document.write(document.cookie);
